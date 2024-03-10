72358214cb5b13be9efc19dcaa8b418dd155e189
dclarke35 <dclarke35@nait.ca>
2024-03-10 16:43:23 -0600

    finish part 2

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+import { getToDoData } from "./models/toDoListModel";
 import { toDoListView } from "./views/toDoListView";
 
-toDoListView();
+async function appInit(){
+    getToDoData()
+    toDoListView()
+}
+
+appInit()
---
+++ js/models/toDoListModel.js
@@ -0,0 +1,25 @@
+import { ref, set, get, push, child, remove, update } from "firebase/database";
+import { db } from "../lib/firebase/config/firebaseInit";
+
+let observers = []
+
+export function subscribe(fn){
+    observers.push(fn)
+    console.log(observers)
+}
+
+export function notify(data){
+    observers.forEach((observer) => observer(data))
+}
+
+export async function getToDoData() {
+    const dbRef = ref(db, 'todos')
+    const response = await get(dbRef)
+    let payload = await response.val()
+    payload = Object.entries(payload)
+    let toDoItems = payload.map((item) =>{
+        return {...item[1], uid: item [0]}
+    })
+
+    notify(toDoItems)
+}
